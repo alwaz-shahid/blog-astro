148c50284cd9f21412aaf5cad62a972063cc51ab
Alwaz Shahid <52915358+Alwaz18@users.noreply.github.com>
2023-04-30 16:42:43 +0500

    tailwind error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
+import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+  ],
+});
