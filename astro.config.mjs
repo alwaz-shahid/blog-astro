5890bf6510a55be10d049ff02c6af056ba86cf44
Alwaz Shahid <52915358+Alwaz18@users.noreply.github.com>
2023-04-30 17:26:13 +0500

    output: "static",

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import vercel from "@astrojs/vercel/static";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
+  output: "static",
   adapter: vercel()
 });
